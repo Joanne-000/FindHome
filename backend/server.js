1bad8a525f20e518b0f80994b8781630a346e85d
Joanne <joannekjy@hotmail.com>
2025-07-22 10:13:04 +0800

    feat:fix error

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://findhomeproperty.netlify.app/",
+  "https://findhomeproperty.netlify.app",
 ];
 
 //connect to postgres
