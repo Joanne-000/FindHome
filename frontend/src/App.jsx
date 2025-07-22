3a172b141718c182b13f840ee602ec310209639f
Joanne <joannekjy@hotmail.com>
2025-07-23 00:11:55 +0800

    feat: amend typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,44 @@
-
-
-import './App.css'
+import "./App.css";
 import debug from "debug";
 import { Routes, Route } from "react-router";
-
 
 import UserDetailForm from "./components/UserDetailForm";
 import SignInForm from "./components/SignInForm";
 import UserProfile from "./components/Profile";
-import EditProfile from './components/EditProfile';
-import ListingsPage from './components/ListingsPage';
-import OneListingPage from './components/OneListingPage';
-import ListingForm from './components/ListingForm';
-import EditListing from './components/EditListing';
-import FavPage from './components/FavPage';
-import NavBar from './components/Navbar';
-import HomePage from './components/HomePage';
-import NotFoundPage from './components/NotFOundPage';
-import CheckOutPage from './components/CheckOutPage';
+import EditProfile from "./components/EditProfile";
+import ListingsPage from "./components/ListingsPage";
+import OneListingPage from "./components/OneListingPage";
+import ListingForm from "./components/ListingForm";
+import EditListing from "./components/EditListing";
+import FavPage from "./components/FavPage";
+import NavBar from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import NotFoundPage from "./components/NotFoundPage";
+import CheckOutPage from "./components/CheckOutPage";
 
 const log = debug("list:App");
 
 function App() {
-
   log("Loading Routes");
   return (
     <>
-    <NavBar/>
-    <Routes>
-    <Route path="/" element={<HomePage/>} />
-        <Route path="/signup" element={<UserDetailForm/>} />
-        <Route path="/signin" element={<SignInForm/>} />
-        <Route path="/listings" element={<ListingsPage/>} />
-        <Route path="/favourites" element={<FavPage/>} />
-        <Route path="/listings/new" element={<ListingForm/>} />
-        <Route path="/listings/:listingId/edit" element={<EditListing/>} />
-        <Route path="/listings/:listingId" element={<OneListingPage/>} />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<UserDetailForm />} />
+        <Route path="/signin" element={<SignInForm />} />
+        <Route path="/listings" element={<ListingsPage />} />
+        <Route path="/favourites" element={<FavPage />} />
+        <Route path="/listings/new" element={<ListingForm />} />
+        <Route path="/listings/:listingId/edit" element={<EditListing />} />
+        <Route path="/listings/:listingId" element={<OneListingPage />} />
         <Route path="/profile/edit" element={<EditProfile />} />
         <Route path="/profile" element={<UserProfile />} />
         <Route path="/checkout" element={<CheckOutPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
